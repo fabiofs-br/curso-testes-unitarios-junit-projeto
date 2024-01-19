7f78e15cad495211d5fd060924608be9ed77fdf0
Fabio Ferreira <fabiofs.br@gmail.com>
2024-01-19 12:36:32 -0300

    3.4. Aplicando a nomenclatura do BDD para nomear métodos de teste

--- src/test/java/com/algaworks/junit/utilidade/FiltroNumerosTest.java
+++ src/test/java/com/algaworks/junit/utilidade/FiltroNumerosTest.java
@@ -11,14 +11,25 @@ import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class FiltroNumerosTest {
-    
+
+    // Given, When, Then
+    // Dado, Quando, Então
     @Test
-    public void deve_retornar_numeros_pares_() {
+    public void Dado_um_lista_de_numero_Quando_filtrar_por_pares_Entao_deve_retornar_apenas_numeros_pares() {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4);
         List<Integer> numerosParesEsperados = Arrays.asList(2, 4);
         List<Integer> resultadoFiltro = FiltroNumeros.numerosPares(numeros);
 
         assertIterableEquals(numerosParesEsperados, resultadoFiltro);
+    }
+
+    @Test
+    public void Dado_um_lista_de_numero_Quando_filtrar_por_pares_Entao_deve_retornar_apenas_numeros_impares() {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4);
+        List<Integer> numerosImparesEsperados = Arrays.asList(1, 3);
+        List<Integer> resultadoFiltro = FiltroNumeros.numerosImpares(numeros);
+
+        assertIterableEquals(numerosImparesEsperados, resultadoFiltro);
     }
 
 }
--- src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -1,6 +1,7 @@
 package com.algaworks.junit.utilidade;
 
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 
@@ -9,54 +10,32 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@DisplayName("Testes no utilitário de saudação")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class SaudacaoUtilTest {
 
     @Test
-    @DisplayName("Deve saudar com bom dia")
-    public void saudarBomDia() {
-        // Arrange
+    public void Dado_um_horario_matutino_Quando_saudar_Entao_deve_retornar_boa_dia() {
         int horaValida = 9;
-
-        // Act
-        String saudacao = saudar(horaValida);
-
-        // Assert
-        assertEquals("Bom dia", saudacao);
-    }
-
-    @Test
-    @DisplayName("Deve sauda com bom dia às 5 horas")
-    public void saudarBomDiaAPartir5h() {
-        int horaValida = 5;
         String saudacao = saudar(horaValida);
         assertEquals("Bom dia", saudacao);
     }
 
-
     @Test
-    public void saudarBoaTarde() {
+    public void Dado_um_horario_vespertino_Quando_saudar_Entao_deve_retornar_boa_tarde() {
         int horaValida = 15;
         String saudacao = saudar(horaValida);
         assertEquals("Boa tarde", saudacao);
     }
 
     @Test
-    public void saudarBoaNoite() {
-        int horaValida = 22;
-        String saudacao = saudar(horaValida);
-        assertEquals("Boa noite", saudacao);
-    }
-
-    @Test
-    public void saudarBoaNoiteAs4h() {
+    public void Dado_um_horario_noturno_Quando_saudar_Entao_deve_retornar_boa_noite() {
         int horaValida = 4;
         String saudacao = saudar(horaValida);
         assertEquals("Boa noite", saudacao);
     }
 
     @Test
-    public void deveLancarException() {
+    public void Dado_uma_hora_invalida_Quando_saudar_Entao_deve_lancar_exception() {
         int horaInvalida = -10;
         Executable chamadaInvalidaDeMetodo = () -> saudar(horaInvalida);
         IllegalArgumentException e = assertThrows(IllegalArgumentException.class, chamadaInvalidaDeMetodo);
@@ -64,7 +43,7 @@ class SaudacaoUtilTest {
     }
 
     @Test
-    public void naoDeveLancarException() {
+    public void Dado_uma_hora_valida_Quando_saudar_Entao_nao_deve_lancar_exception() {
         int horaValida = 0;
         Executable chamadaValidaDeMethodo = () -> saudar(horaValida);
         assertDoesNotThrow(chamadaValidaDeMethodo);
